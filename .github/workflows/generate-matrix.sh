0540d3017f0c1f0f5fe5a25a9167db0615f6138b
zenobit <zenobit@disroot.org>
2024-06-28 16:21:02 +0200

    100apps matrix

---
+++ .github/workflows/generate-matrix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+randomAppsList=$(cut -d' ' -f2 < /opt/am/x86_64-apps | shuf -n 100)
+apps=$(echo "$randomAppsList" | jq -R -s -c 'split("\n")[:-1]')
+echo "::set-output name=matrix::${apps}"
