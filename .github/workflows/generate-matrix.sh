84444a01815af8d543ad46431c4e698b0847304f
zenobit <zenobit@disroot.org>
2024-07-02 15:41:30 +0200

    cccc

--- .github/workflows/generate-matrix.sh
+++ .github/workflows/generate-matrix.sh
@@ -2,4 +2,9 @@
 
 randomAppsList=$(cut -d' ' -f2 < /opt/am/x86_64-apps | shuf -n 100)
 apps=$(echo "$randomAppsList" | jq -R -s -c 'split("\n")[:-1]')
-echo "name=matrix::${apps}" >> $GITHUB_OUTPUT
+
+# Convert bash array to JSON array
+matrix=$(printf '%s\n' "${apps[@]}" | jq -R . | jq -s .)
+
+# Output the matrix in a format GitHub Actions can use
+echo "::set-output name=matrix::$matrix" #>> $GITHUB_OUTPUT
